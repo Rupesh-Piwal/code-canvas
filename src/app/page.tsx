c03954da930e6165bdde7db97cb6d2b505c4a319
rupesh <rpiwal02@gmail.com>
2024-09-13 01:07:51 +0530

    code editor ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import CodeEditor from "@/components/CodeEditor";
+import { backgrounds, languages, themes } from "@/utils/utilities";
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const editorRef = useRef(null);
+  const [language, setLanguage] = useState(languages[0].name);
+  const [theme, setTheme] = useState(themes[0]);
+  const [background, setBackground] = useState(backgrounds[0]);
+  const [activeIcon, setActiveIcon] = useState(languages[0].icon);
+  const [paddings, setPaddings] = useState(["1rem", "2rem", "3rem", "4rem"]);
+  const [currentPadding, setCurrentPadding] = useState(paddings[2]);
   return (
     <div>
-      <CodeEditor />
+      <div className="code-editor-ref mt-[14rem]" ref={editorRef}>
+        <CodeEditor
+          language={language}
+          theme={theme}
+          background={background}
+          icon={activeIcon}
+          currentPadding={currentPadding}
+        />
+      </div>
     </div>
   );
 }
