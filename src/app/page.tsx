1c3da2fef21668d1657f2300ec56e0fa9f1ffd5a
rupesh <rpiwal02@gmail.com>
2024-09-13 00:00:14 +0530

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import CodeEditor from "@/components/CodeEditor";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <CodeEditor />
+    </div>
+  );
 }
--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,5 +1,23 @@
+"use client";
+import { Resizable } from "re-resizable";
+import AceEditor from "react-ace";
+
 const CodeEditor = () => {
-  return <div>CodeEditor</div>;
+  return (
+    <Resizable
+      minHeight={466}
+      minWidth={510}
+      maxWidth={1000}
+      //   defaultSize={{
+      //     width: width,
+      //     height: height || 500,
+      //   }}
+    >
+      <div>
+        <AceEditor />
+      </div>
+    </Resizable>
+  );
 };
 
 export default CodeEditor;
