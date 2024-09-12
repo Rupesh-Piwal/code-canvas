4d13c6013a554f91fe32dc5b9e585b5f17378dbf
rupesh <rpiwal02@gmail.com>
2024-09-13 01:21:06 +0530

    imported components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 "use client";
+import BackgroundSelector from "@/components/BackgroundSelector";
 import CodeEditor from "@/components/CodeEditor";
+import LanguageSelector from "@/components/LanguageSelector";
+import PaddingSelector from "@/components/PaddingSelector";
+import ThemeSelector from "@/components/ThemeSelector";
 import { backgrounds, languages, themes } from "@/utils/utilities";
 import { useRef, useState } from "react";
 
@@ -12,7 +16,44 @@ export default function Home() {
   const [paddings, setPaddings] = useState(["1rem", "2rem", "3rem", "4rem"]);
   const [currentPadding, setCurrentPadding] = useState(paddings[2]);
   return (
-    <div>
+    <main className=" h-[100vh] flex flex-col items-center justify-between">
+      <header
+        className="mt-6 flex gap-6 w-[940px] p-5 fixed top-0 left-1/2 translate-x-[-50%]
+         z-10 bg-[#191919] rounded border border-[#3C3C3C] shadow-md"
+      >
+        <LanguageSelector
+          // language={language}
+          // setLanguage={setLanguage}
+          // seActiveIcon={setActiveIcon}
+        />
+
+        <ThemeSelector 
+        // theme={theme} setTheme={setTheme}
+         />
+
+        <BackgroundSelector
+          // background={background}
+          // setBackground={setBackground}
+        />
+
+        <PaddingSelector
+          // paddings={paddings}
+          // currentPadding={currentPadding}
+          // setCurrentPadding={setCurrentPadding}
+        />
+
+        <div className="export-btn self-center ml-auto">
+          <button
+            className="flex items-center gap-3 py-2 px-3 bg-blue-400 rounded-md text-sm text-blue-400 
+              font-medium bg-opacity-10 hover:bg-opacity-80 hover:text-slate-50 ease-in-out transition-all 
+              duration-300"
+            // onClick={exportPng}
+          >
+            {/* <Download /> */}
+            Export PNG
+          </button>
+        </div>
+      </header>
       <div className="code-editor-ref mt-[14rem]" ref={editorRef}>
         <CodeEditor
           language={language}
@@ -22,6 +63,6 @@ export default function Home() {
           currentPadding={currentPadding}
         />
       </div>
-    </div>
+    </main>
   );
 }
