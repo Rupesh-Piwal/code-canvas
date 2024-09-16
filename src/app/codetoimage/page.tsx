491049e2caa34183d7c15bc05a9da998e0a9d983
rupesh <rpiwal02@gmail.com>
2024-09-16 10:46:11 +0530

    feature: export png

--- src/app/codetoimage/page.tsx
+++ src/app/codetoimage/page.tsx
@@ -6,6 +6,8 @@ import LanguageSelector from "@/components/LanguageSelector";
 import PaddingSelector from "@/components/PaddingSelector";
 import ThemeSelector from "@/components/ThemeSelector";
 import { backgrounds, languages, themes } from "@/utils/utilities";
+import html2canvas from "html2canvas";
+import { Download } from "lucide-react";
 import { useRef, useState } from "react";
 
 // import { getExtension, initialCode } from "@/utils/utilities";
@@ -18,6 +20,43 @@ const page = () => {
   const [activeIcon, setActiveIcon] = useState(languages[0].icon);
   const [paddings, setPaddings] = useState(["1rem", "2rem", "3rem", "4rem"]);
   const [currentPadding, setCurrentPadding] = useState(paddings[2]);
+
+  const exportPng = async () => {
+    const editorElem = editorRef.current;
+
+    if (editorElem) {
+      //hide elemnets
+      const handleElems = document.querySelectorAll(".handle") as any;
+      const cursorElem = document.querySelector(".ace_cursor") as any;
+      const codetitle = document.querySelector(".code-title") as any;
+      const codeEditor = document.querySelector(".ace_editor") as any;
+
+      handleElems.forEach((elem: any) => {
+        elem.style.display = "none";
+      });
+      cursorElem.style.display = "none";
+      codetitle.style.boxShadow = "none";
+      codeEditor.style.boxShadow = "none";
+
+      const canvas = await html2canvas(editorElem);
+      const image = canvas
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream");
+
+      const link = document.createElement("a");
+      link.download = "code.png";
+      link.href = image;
+      link.click();
+
+      //show elements
+      handleElems.forEach((elem: any) => {
+        elem.style.display = "block";
+      });
+      cursorElem.style.display = "block";
+      codetitle.style.boxShadow = "0 3px 10px rgba(0, 0, 0, 0.2)";
+      codeEditor.style.boxShadow = "2px 3px 10px rgba(0, 0, 0, 0.2)";
+    }
+  };
 
   return (
     <main className=" h-[100vh] flex flex-col items-center justify-between">
@@ -49,9 +88,9 @@ const page = () => {
             className="flex items-center gap-3 py-2 px-3 bg-blue-400 rounded-md text-sm text-blue-400 
             font-medium bg-opacity-10 hover:bg-opacity-80 hover:text-slate-50 ease-in-out transition-all 
             duration-300"
-            // onClick={exportPng}
+            onClick={exportPng}
           >
-            {/* <Download /> */}
+            <Download />
             Export PNG
           </button>
         </div>
